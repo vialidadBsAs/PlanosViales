ab086eb5a7e71f4cf59a764528a6b060dc9bb3f6
guadaevequoz <guadalupe.evequoz@gmail.com>
2022-06-06 09:57:18 -0300

    Elimine algo para poder commitear

--- Sistema de planos/Controllers/WeatherForecastController.cs
+++ Sistema de planos/Controllers/WeatherForecastController.cs
@@ -24,8 +24,6 @@ namespace Sistema_de_planos.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
