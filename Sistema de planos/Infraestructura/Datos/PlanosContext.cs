5fd0f63c245462ee73dc38974bbf27c45b875e95
guadaevequoz <guadalupe.evequoz@gmail.com>
2022-07-12 11:06:44 -0300

    puede buscar por partidoNombre sin tener que escribir todo el partido

--- Sistema de planos/Infraestructura/Datos/PlanosContext.cs
+++ Sistema de planos/Infraestructura/Datos/PlanosContext.cs
@@ -39,7 +39,7 @@ namespace Sistema_de_planos.Infraestructura.Datos
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=SI-DESARROLLO05\\SQLEXPRESS; Database=Planos; Integrated Security=true; Multiple Active Result Sets=true;");
+                optionsBuilder.UseSqlServer("Server=SI-SOPORTE22\\SQLEXPRESS; Database=Planos; Integrated Security=true; Multiple Active Result Sets=true;");
             }
         }
 
